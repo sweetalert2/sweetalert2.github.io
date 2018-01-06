99ec20737593d843cc411ac80ad57b2f5656ab89
Limon Monte <limon.monte@gmail.com>
2018-01-06 16:17:18 +0200

    Separate color and Carbon Ads styles

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,20 @@ const autoprefix = require('gulp-autoprefixer')
 const sassLint = require('gulp-sass-lint')
 
 gulp.task('sass', ['sass-lint'], (cb) => {
-  gulp.src('styles/styles.scss')
+  gulp.src([
+    'styles/styles.scss',
+    'styles/carbon-ads.scss'
+  ])
     .pipe(sass())
     .pipe(autoprefix())
     .pipe(gulp.dest('styles'))
 })
 
 gulp.task('sass-lint', () => {
-  return gulp.src('styles/styles.scss')
+  return gulp.src([
+    'styles/styles.scss',
+    'styles/carbon-ads.scss'
+  ])
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
@@ -20,5 +26,8 @@ gulp.task('sass-lint', () => {
 gulp.task('default', ['sass'])
 
 gulp.task('watch', () => {
-  gulp.watch('styles/styles.scss', ['sass'])
+  gulp.watch([
+    'styles/styles.scss',
+    'styles/carbon-ads.scss'
+  ], ['sass'])
 })
