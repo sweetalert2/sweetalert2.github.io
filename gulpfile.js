898c4dfcdae9a77534bd38c3fe93db342de6f685
Limon Monte <limonte@users.noreply.github.com>
2018-01-20 17:31:08 +0200

    Add browsersync for better DX

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const autoprefix = require('gulp-autoprefixer')
 const sassLint = require('gulp-sass-lint')
+const browserSync = require('browser-sync').create()
 
 gulp.task('sass', ['sass-lint'], (cb) => {
-  gulp.src([
+  return gulp.src([
     'styles/styles.scss',
     'styles/carbon-ads.scss'
   ])
@@ -26,8 +27,20 @@ gulp.task('sass-lint', () => {
 gulp.task('default', ['sass'])
 
 gulp.task('watch', () => {
+  browserSync.init({
+    port: 8080,
+    notify: false,
+    reloadOnRestart: true,
+    https: false,
+    server: ['./']
+  })
+
   gulp.watch([
-    'styles/styles.scss',
-    'styles/carbon-ads.scss'
+    'index.html',
+    'styles/*.css'
+  ]).on('change', browserSync.reload)
+
+  gulp.watch([
+    'styles/*.scss',
   ], ['sass'])
 })
