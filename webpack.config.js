5384026382172743483a86800c7547d4010af99c
Limon Monte <limonte@users.noreply.github.com>
2018-01-29 11:09:47 +0200

    Add transpiling to ES5 with (via webpack) (#6)

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: ['babel-polyfill', './js/main.js'],
+  output: {
+    filename: './js/bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
