1060a82a4d724c52c8075ab18a2925d7aebbd327
Limon Monte <limonte@users.noreply.github.com>
2018-02-01 15:24:47 +0200

    Use Global site tag (gtag.js) - Google Analytics

--- js/main.js
+++ js/main.js
@@ -552,14 +552,3 @@ function setCarbonFooter () {
 
 var observer = new MutationObserver(setCarbonFooter)
 observer.observe($('.carbonads-wrapper .bsa-cpc')[0], {childList: true})
-
-// Google Analytics
-/* eslint-disable */
-if (typeof ga !== 'undeifned') {
-  (function (i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function (){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
-  ga('create', 'UA-83618163-1', 'auto')
-  ga('send', 'pageview')
-}
