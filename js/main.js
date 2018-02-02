3c9707540e8b9484f292d1c289762fa0472d88ee
Limon Monte <limonte@users.noreply.github.com>
2018-02-02 13:50:36 +0200

    chore(docs): document new backdrop feature, add the Nyan Cat example (#7)

--- js/main.js
+++ js/main.js
@@ -260,7 +260,13 @@ $('.examples .custom-width-padding-background button').on('click', () => {
     title: 'Custom width, padding, background.',
     width: 600,
     padding: 100,
-    background: '#fff url(https://bit.ly/1Nqn9HU)'
+    background: '#fff url(/images/trees.png)',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      url("/images/nyan-cat.gif")
+      center left
+      no-repeat
+    `
   })
 })
 
