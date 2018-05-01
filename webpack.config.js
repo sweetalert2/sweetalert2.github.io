036b45270475a7a98ac0113936a3704bc1c23620
Limon Monte <limon.monte@gmail.com>
2018-05-01 13:54:12 +0300

    Simplify service workers usage

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack')
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 module.exports = {
   entry: ['babel-polyfill', './js/main.js'],
@@ -18,38 +17,6 @@ module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
       'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
-    }),
-    new SWPrecacheWebpackPlugin({
-      staticFileGlobs: [
-        'images/**/*'
-      ],
-      runtimeCaching: [
-        {
-          urlPattern: '/',
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: '/styles/**/*.css',
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: '/js/bundle.js',
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/npm\/.*/,
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/,
-          handler: 'networkFirst'
-        },
-        {
-          urlPattern: /^https:\/\/unsplash\.it\/400\/200\/\?random/,
-          handler: 'networkFirst'
-        }
-      ]
-      // minify: true
     })
   ]
 }
