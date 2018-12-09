2fe28291ba6bb43fd3a53743851824f373f222eb
gverni <g.verni@gmail.com>
2018-12-09 15:41:26 +0000

    Added swal timer APIs documentation and sample (#42)
    * Added swal timer APIs documentation
    * Added swal timer APIs sample
    * Set lang of timer functions example to js, hide it for mobile devices

--- src/app.js
+++ src/app.js
@@ -575,6 +575,62 @@ document.querySelector('.examples .dynamic-queue button').onclick = () => {
   ])
 }
 
+document.querySelector('.examples .timer-functions button').onclick = () => {
+  let timerInterval
+  Swal({
+    title: 'Auto close alert!',
+    html: 'I will close in <strong></strong> seconds.<br/><br/>' +
+    '<button id="increase" class="btn btn-warning">I need 5 more seconds!</button><br/>' +
+    '<button id="stop" class="btn btn-danger">Please stop the timer!!</button><br/>' +
+    '<button id="resume" class="btn btn-success" disabled>Phew... you can restart now!</button><br/>' +
+    '<button id="toggle" class="btn btn-primary">Toggle</button>',
+    timer: 10000,
+    onOpen: () => {
+      const content = Swal.getContent()
+      const $ = content.querySelector.bind(content)
+
+      const stop = $('#stop')
+      const resume = $('#resume')
+      const toggle = $('#toggle')
+      const increase = $('#increase')
+
+      Swal.showLoading()
+
+      function toggleButtons () {
+        stop.disabled = !Swal.isTimerRunning()
+        resume.disabled = Swal.isTimerRunning()
+      }
+
+      stop.addEventListener('click', () => {
+        Swal.stopTimer()
+        toggleButtons()
+      })
+
+      resume.addEventListener('click', () => {
+        Swal.resumeTimer()
+        toggleButtons()
+      })
+
+      toggle.addEventListener('click', () => {
+        Swal.toggleTimer()
+        toggleButtons()
+      })
+
+      increase.addEventListener('click', () => {
+        Swal.increaseTimer(5000)
+      })
+      timerInterval = setInterval(() => {
+        Swal.getContent().querySelector('strong')
+          .textContent = (Swal.getTimerLeft() / 1000)
+            .toFixed(0)
+      }, 100)
+    },
+    onClose: () => {
+      clearInterval(timerInterval)
+    }
+  })
+}
+
 document.querySelector('.examples .mixin button').onclick = () => {
   const toast = Swal.mixin({
     toast: true,
