00a7ee415c1d4c3d3d27bfc675b323115acc4424
Limon Monte <limonte@users.noreply.github.com>
2020-09-29 00:51:49 +0300

    Add Bootstrap 4 example

--- recipe-gallery/.eslintrc.js
+++ recipe-gallery/.eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   rules: {
     'indent': 0,
     '@typescript-eslint/indent': 0,
+    '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-unused-vars': 0,
   },
   globals: {
-    'grecaptcha': 'readonly'
+    '$': 'readonly',
+    'grecaptcha': 'readonly',
   }
 }
