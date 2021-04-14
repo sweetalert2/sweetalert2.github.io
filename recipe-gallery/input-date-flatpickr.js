047f5e9823da871b2244bcc125f6b55e9ec2eb0a
Limon Monte <limon.monte@gmail.com>
2021-04-14 11:01:53 +0300

    Add stopKeydownPropagation: false to flatpickr example

--- recipe-gallery/input-date-flatpickr.js
+++ recipe-gallery/input-date-flatpickr.js
@@ -4,6 +4,7 @@ let flatpickrInstance
 Swal.fire({
   title: 'Please enter departure date',
   html: '<input class="swal2-input" id="expiry-date">',
+  stopKeydownPropagation: false,
   preConfirm: () => {
     if (flatpickrInstance.selectedDates[0] < new Date()) {
       Swal.showValidationMessage(`The departure date can't be in the past`)
