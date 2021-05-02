dc712279761cc89805591fa5f7f04f16fcdfd0cb
Limon Monte <limon.monte@gmail.com>
2021-05-02 21:02:43 +0300

    use gulp-dart-sass instead of deprecated gulp-sass
    https://sass-lang.com/blog/libsass-is-deprecated

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-dart-sass')
 const autoprefix = require('gulp-autoprefixer')
 const browserSync = require('browser-sync').create()
 const webpackStream = require('webpack-stream')
