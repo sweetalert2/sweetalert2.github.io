50ede5856976c816df24ebc010d4454160ccd06f
Limon Monte <limon.monte@gmail.com>
2021-12-05 12:40:08 +0200

    prevent backdrop blinking in Queue recipe

--- recipe-gallery/queue-with-progress-steps.js
+++ recipe-gallery/queue-with-progress-steps.js
@@ -1,11 +1,28 @@
 window.showExample = async () => {
 const steps = ['1', '2', '3']
-const swalQueue = Swal.mixin({
+const Queue = Swal.mixin({
   progressSteps: steps,
   confirmButtonText: 'Next >',
+  // optional classes to avoid backdrop blinking between steps
+  showClass: { backdrop: 'swal2-noanimation' },
+  hideClass: { backdrop: 'swal2-noanimation' }
 })
 
-await swalQueue.fire({ title: 'Uno', currentProgressStep: 0 })
-await swalQueue.fire({ title: 'Dos', currentProgressStep: 1 })
-await swalQueue.fire({ title: 'Tres', currentProgressStep: 2, confirmButtonText: 'OK' })
+await Queue.fire({
+  title: 'Uno',
+  currentProgressStep: 0,
+  // optional class to show fade-in backdrop animation which was disabled in Queue mixin
+  showClass: { backdrop: 'swal2-noanimation' },
+})
+await Queue.fire({
+  title: 'Dos',
+  currentProgressStep: 1
+})
+await Queue.fire({
+  title: 'Tres',
+  currentProgressStep: 2,
+  confirmButtonText: 'OK',
+  // optional class to show fade-out backdrop animation which was disabled in Queue mixin
+  showClass: { backdrop: 'swal2-noanimation' },
+})
 }
