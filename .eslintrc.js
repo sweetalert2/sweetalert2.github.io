921594794f195edf86db282f45c0b935c16d93a8
Limon Monte <limonte@users.noreply.github.com>
2022-02-05 11:11:09 +0200

    improve sidebar styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: [
-    '@sweetalert2/eslint-config'
-  ],
+  extends: ['@sweetalert2/eslint-config'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+  },
   globals: {
     Swal: true,
-    frenchkiss: true
-  }
+    frenchkiss: true,
+  },
 }
