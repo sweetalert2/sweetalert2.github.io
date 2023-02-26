ee8a3053f9139fa64909d8fb5a64a054e034139a
Limon Monte <limonte@users.noreply.github.com>
2023-02-26 17:41:02 +0200

    chore: improve daepicker example

--- recipe-gallery/input-datepicker.js
+++ recipe-gallery/input-datepicker.js
@@ -7,15 +7,18 @@ Swal.fire({
   inputValue: new Date().toISOString(),
   stopKeydownPropagation: false,
   preConfirm: () => {
-    if (datepicker.getDate() < new Date(new Date().setHours(0,0,0,0))) {
+    if (datepicker.getDate() < new Date(new Date().setHours(0, 0, 0, 0))) {
       Swal.showValidationMessage(`The departure date can't be in the past`)
     }
     return datepicker.getDate()
   },
   didOpen: () => {
-    datepicker = new Pikaday( {field: Swal.getInput() });
-    setTimeout(() => datepicker.show(), 400); // show calendar after showing animation
-  }
+    datepicker = new Pikaday({ field: Swal.getInput() })
+    setTimeout(() => datepicker.show(), 400) // show calendar after showing animation
+  },
+  didClose: () => {
+    datepicker.destroy()
+  },
 }).then((result) => {
   console.log(result.value)
 })
