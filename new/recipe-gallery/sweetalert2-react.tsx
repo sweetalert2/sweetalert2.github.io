74516787bc77452e19a712a81450f421d5c9eccc
limonte <6059356+limonte@users.noreply.github.com>
2023-10-16 07:06:14 +0000

    Deploying to gh-pages from @ sweetalert2/sweetalert2.github.io@6c8a90a95ead7841f84dfb519a79116d635bfbd1 🚀

---
+++ new/recipe-gallery/sweetalert2-react.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+
+export function RecipeReact() {
+  return (
+    <>
+      <h1>Recipe React</h1>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<RecipeReact />)
