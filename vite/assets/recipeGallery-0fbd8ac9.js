1796406962bead5f89cc4e22ec3346c64ee6eede
limonte <6059356+limonte@users.noreply.github.com>
2023-10-16 07:56:45 +0000

    Deploying to gh-pages from @ sweetalert2/sweetalert2.github.io@b2dd72e56116bb759c5d3c19302cce09a33b1c12 🚀

---
+++ vite/assets/recipeGallery-0fbd8ac9.js
@@ -0,0 +1,8 @@
+import { c as client, j as jsxRuntimeExports } from "./client-836a600e.js";
+function RecipeGallery() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+    /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "RecipeGallery" }),
+    /* @__PURE__ */ jsxRuntimeExports.jsx("a", { href: "./sweetalert2-react.html", children: "react" })
+  ] });
+}
+client.createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxRuntimeExports.jsx(RecipeGallery, {}));
---
+++ vite/assets/recipeReact-e41f2fc6.js
@@ -0,0 +1,5 @@
+import { c as client, j as jsxRuntimeExports } from "./client-836a600e.js";
+function RecipeReact() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "Recipe React" }) });
+}
+client.createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxRuntimeExports.jsx(RecipeReact, {}));
