278948268aed4dc86044820ea39b58f15b60ae66
limonte <6059356+limonte@users.noreply.github.com>
2024-10-13 15:24:22 +0000

    Deploying to gh-pages from @ sweetalert2/sweetalert2.github.io@8335886281a9ecd632db2901d37c08729d036fd3 🚀

---
+++ assets/custom-icon-styles-DjK9iNKt.js
@@ -0,0 +1,3 @@
+const customIconStyles = ".rotate-y {\n  animation: rotateY 1s infinite linear;\n}\n\n@keyframes rotateY {\n  to {\n    transform: rotateY(360deg);\n  }\n}\n";
+
+export { customIconStyles as default };
