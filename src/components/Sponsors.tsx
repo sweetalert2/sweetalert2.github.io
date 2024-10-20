0d4d469f28478872cde511c27ff11334c8299baa
Limon Monte <6059356+limonte@users.noreply.github.com>
2024-10-20 19:17:17 +0300

    chore: add huntlocals to sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -129,6 +129,13 @@ export function Sponsors() {
 
       <div className="sponsors">
         <div>
+          <a href="https://huntlocals.com/" target="_blank" rel="noopener" aria-label="HuntLocals">
+            <img src="/images/sponsors/huntlocals.png" alt="HuntLocals" />
+            <br />
+            HuntLocals
+          </a>
+        </div>
+        <div>
           <a href="https://siteshartbandi.co/" target="_blank" rel="noopener" aria-label="سایت شرط بندی">
             <img src="/images/sponsors/siteshartbandi.png" alt="سایت شرط بندی" />
             <br />
