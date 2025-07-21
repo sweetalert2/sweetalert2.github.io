e6032634a69a2924d2ded8802fe4003b7b86afc3
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-07-21 17:27:56 +0300

    chore: rm impact and careerfoundry

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -16,52 +16,6 @@ export function Partners() {
       ),
       imageUrl: '/images/hostinger.png',
     },
-
-    // CareerFoundry
-    {
-      link: 'https://careerfoundry.com/en/referral_registrations/new?referral=4rARax01',
-      title: (
-        <b>
-          Get 10% discount <span className="nowrap"> on CareerFoundry courses!</span>
-        </b>
-      ),
-      description: (
-        <>
-          Career-change programs with a job guarantee.
-          <br />
-          Launch a career in tech that lasts 🚀
-        </>
-      ),
-      imageUrl: '/images/careerfoundry.png',
-    },
-
-    // Coursera (via Impact)
-    {
-      link: 'https://imp.i384100.net/kOJnXz',
-      title: <b>Learn web development on Coursera!</b>,
-      description: (
-        <>
-          Flexible learning paths for your goals.
-          <br />
-          Unlock your potential now! 🚀
-        </>
-      ),
-      imageUrl: '/images/coursera.png',
-    },
-
-    // SentryPC (via Impact)
-    {
-      link: 'https://sentrypc.7eer.net/e1ajMj',
-      title: <b>Monitor. Filter. Control.</b>,
-      description: (
-        <>
-          Know everything your users do on their PC
-          <br />
-          and mobile devices on the Internet.
-        </>
-      ),
-      imageUrl: '/images/sentrypc.png',
-    },
   ]
 
   const partner = partners[Math.floor(Math.random() * partners.length)]
