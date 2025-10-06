2b2208d57aceaf71f3ba8980d4d5eb3184a34b17
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-10-06 22:58:01 +0300

    feat: add bootstrap-4 theme

--- src/utils/themableSwal.tsx
+++ src/utils/themableSwal.tsx
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2'
 import 'sweetalert2/themes/borderless.css'
 import 'sweetalert2/themes/minimal.css'
 import 'sweetalert2/themes/bulma.css'
+import 'sweetalert2/themes/bootstrap-4.css'
 
 declare global {
   interface Window {
@@ -35,6 +36,7 @@ export const ThemeSelector = () => {
           <option value="light">Default (light)</option>
           <option value="dark">Dark</option>
           <option value="auto">Auto</option>
+          <option value="bootstrap-4">Bootstrap 4</option>
           <option value="minimal">Minimal</option>
           <option value="borderless">Borderless</option>
           <option value="bulma">Bulma</option>
