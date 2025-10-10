eebdf8b188d1cb319162dbf9caccb88527fa9491
Limon Monte <6059356+limonte@users.noreply.github.com>
2025-10-10 23:17:35 +0300

    feat: add Bootstrap 5 theme (#225)

--- src/utils/themableSwal.tsx
+++ src/utils/themableSwal.tsx
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2'
 import 'sweetalert2/themes/borderless.css'
 import 'sweetalert2/themes/minimal.css'
 import 'sweetalert2/themes/bulma.css'
+import 'sweetalert2/themes/bootstrap-5.css'
 import 'sweetalert2/themes/bootstrap-4.css'
 import 'sweetalert2/themes/material-ui.css'
 
@@ -37,6 +38,7 @@ export const ThemeSelector = () => {
           <option value="light">Default (light)</option>
           <option value="dark">Dark</option>
           <option value="auto">Auto</option>
+          <option value="bootstrap-5">Bootstrap 5</option>
           <option value="bootstrap-4">Bootstrap 4</option>
           <option value="material-ui">Material UI</option>
           <option value="minimal">Minimal</option>
