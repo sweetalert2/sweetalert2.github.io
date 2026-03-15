067c2bb814b85c3bfda75da610d37d4cf9b845eb
limonte <6059356+limonte@users.noreply.github.com>
2026-03-15 14:32:18 +0000

    Deploying to gh-pages from @ sweetalert2/sweetalert2.github.io@49160eb33000b60bf6f99f8a8adc97c59cfdb8db 🚀

---
+++ assets/Prtnr-DImRzYcr.js
@@ -0,0 +1,7 @@
+import { j as jsxRuntimeExports } from './index-B3WSj48O.js';
+
+function Prtnr() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, {});
+}
+
+export { Prtnr as P };
