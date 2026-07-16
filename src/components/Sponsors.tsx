9f8784650423eb627001cea5d587bbc10508a18e
Limon Monte <6059356+limonte@users.noreply.github.com>
2026-07-16 13:39:28 +0300

    chore: upd hismith target

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -169,7 +169,7 @@ export function Sponsors() {
           </a>
         </div>
         <div>
-          <a href="https://www.hismith.co.uk/" target="_blank" rel="noopener" aria-label="Hismith">
+          <a href="https://www.hismith.com/en/" target="_blank" rel="noopener" aria-label="Hismith">
             <img src="/images/sponsors/hismith.png" alt="Hismith" />
             <br />
             Hismith
